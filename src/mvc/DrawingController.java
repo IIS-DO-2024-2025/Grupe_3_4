d60d7ce639e2c8aba587873a651692cbe3ebd072
staka99 <marko.staka12@gmail.com>
2024-11-05 00:04:47 +0100

    Vezbe 4 - MVC

---
+++ src/mvc/DrawingController.java
@@ -0,0 +1,24 @@
+package mvc;
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+
+public class DrawingController {
+	
+	private DrawingModel model;
+	private DrawingFrame frame;
+	
+	public DrawingController(DrawingModel model, DrawingFrame frame) {
+		this.model = model;
+		this.frame = frame;
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		Point p = new Point(e.getX(), e.getY(), Color.BLUE);
+		model.add(p);
+		frame.repaint();
+	}
+	
+	
+
+}
